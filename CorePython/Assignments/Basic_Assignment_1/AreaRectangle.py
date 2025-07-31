bc7e8011bda955e562afd8a2f4fae78a7bab9e3c
abhishekswalikar07 <walikarabhishek85@gmail.com>
2025-07-31 21:44:05 +0530

    first commit

---
+++ CorePython/Assignments/Basic_Assignment_1/AreaRectangle.py
@@ -0,0 +1,6 @@
+# Write a program to calculat area of rectangle
+
+l=int(input("Enter length of rectangle:"))
+b=int(input("Enter breadth of rectangle:"))
+area=l*b
+print(f'Area of rectangle is {area}')
