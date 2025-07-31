8bfca38059ef09427ab10bb1941873a6d559b7f5
Abhishek Walikar <walikarabhishek85@gmail.com>
2025-07-31 21:34:20 +0530

    Delete Test1 directory

--- Test1/DistanceConvert.py
+++
@@ -1,7 +0,0 @@
-km=int(input("enter total km value:"))
-
-mtr=km*1000
-
-cm=km*1000*100
-
-print(f'{km} km in meter is {mtr} meter and in centimeter is {cm} cm.')
--- Test1/SimpleInterest.py
+++
@@ -1,7 +0,0 @@
-P=float(input("Enter principle value:"))
-R=float(input("Enter rate value:"))
-t=float(input("Enter time in year:"))
-
-SI=(P*R*t)/100
-
-print(f'Simple interest is {SI}')
