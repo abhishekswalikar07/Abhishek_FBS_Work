3c8eb92acd7d870bd808afdd9bb0ac382053f43b
abhishekswalikar07 <walikarabhishek85@gmail.com>
2025-08-06 21:55:34 +0530

    code updated for digit program

--- CorePython/Looping/While/digit.py
+++ CorePython/Looping/While/digit.py
@@ -1,6 +1,10 @@
 num=int(input("Enter number:"))
 temp=num
+sum = 0
+print("Digits are:")
 while temp>0:
     d=temp%10
     print(d)
-    temp=temp//10                                                   
+    sum += d
+    temp=temp//10
+print("Sum of digits:", sum)
