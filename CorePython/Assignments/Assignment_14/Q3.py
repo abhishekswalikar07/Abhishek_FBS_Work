8ef9a73ad755919acbcd67e24ac4071620a46e01
abhishekswalikar07 <walikarabhishek85@gmail.com>
2025-09-09 11:01:01 +0530

    Test 5 completed and some modifications done in ass 14 q3

--- CorePython/Assignments/Assignment_14/Q3.py
+++ CorePython/Assignments/Assignment_14/Q3.py
@@ -2,7 +2,7 @@
 # frequency of occurrence from a given list of strings. Use Python set
 # data type.
 
-texts = ["apple orange apple", "banana apple orange", "banana grape"]
+texts = ["apple orange apple", "banana apple" ,"orange banana grape"]
 
 words = []
 for sentence in texts:
@@ -12,3 +12,13 @@ unique_words = set(words)
 
 for word in unique_words:
     print(f"{word} -> {words.count(word)}")
+
+# # li=texts.split()
+
+# un_words=set()
+
+# for word in texts:
+#     if word not in un_words:
+#         un_words.add(word)
+        
+# print(un_words)
